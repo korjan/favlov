e46988e6452b4aac00e59b7e5d78d68b6ca74a24
korjan <korjanvanwieringen@gmail.com>
2016-04-29 22:23:22 +0200

    another token

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/webhook/', function (req, res) {
     }
     if (event.postback) {
      text = JSON.stringify(event.postback);
-     sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+     sendTextMessage(sender, "Postback received: "+text.substring(0, 200), authtoken);
      continue;
    }
   }
