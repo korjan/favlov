4988a9aaef08a7e3584e29ba9eaee49cb36132dc
korjan <korjanvanwieringen@gmail.com>
2016-04-29 21:15:43 +0200

    Use a body parser

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
